12f35259464e4fd939e1cb7ddce147167b59f6a9
Simone Veitch <simone.veitch@gmail.com>
2024-05-15 12:49:09 +1000

    Completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+  const [filteredListings, setFilteredListings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) => r.json())
+      .then((data) => {
+        setListings(data);
+        setFilteredListings(data);
+      });
+  }, []);
+
+  function handleSearch(query) {
+    const filtered = listings.filter((listing) =>
+      listing.description.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredListings(filtered);
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={handleSearch}/>
+      <ListingsContainer listings={listings} filteredListings={filteredListings} setFilteredListings={setFilteredListings} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,39 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listings, filteredListings, setFilteredListings }) {
+
+function handleFavoriteClick(id) {
+    setFilteredListings((prevListings) =>
+      prevListings.map((listing) =>
+        listing.id === id ? { ...listing, isFavorite: !listing.isFavorite } : listing
+      )
+    );
+  }
+
+  function handleDeleteListing(id) {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        setFilteredListings((prevListings) => prevListings.filter((listing) => listing.id !== id));
+      });
+  }
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {filteredListings.map((listing) => (
+          <ListingCard 
+          key={listing.id}
+          description={listing.description}
+            image={listing.image}
+            location={listing.location}
+            isFavorite={listing.isFavorite}
+            onFavoriteClick={() => handleFavoriteClick(listing.id)}
+            onDeleteListing={() => handleDeleteListing(listing.id)}
+          />
+        ))}
       </ul>
     </main>
   );
